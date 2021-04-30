317f0f686dd8069857598e8ff509f24bfa34f710
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2021-05-01 00:22:54 +0200

    adding neasted function and finishing the assignment

--- assignmentNumeric/DividedDiff.m
+++ assignmentNumeric/DividedDiff.m
@@ -2,23 +2,24 @@
 ## Created: 2021-04-27
 
 function px = DividedDiff (x,y);
- px = NaN(columns(x),columns(x)+1)
+ px = NaN(columns(x),columns(x)+1);
  space = 1;
  fxx = @(row,col,px,space) (px(row+1,col-1)-px(row,col-1))/( x(row+space)-x(row));  
+ 
+ ##filling the first two col with inputs
  for (i=1 : columns(x));
     px(i,1) = x(i);  
     px(i,2) = y(i);
  endfor
  
+ ## using the divided diffrence for caculates other cols
  rowReduce =  columns(x)-1;
- for (a=3 : columns(x)+1);  %col
-   for(b=1 : rowReduce); %row
+ for (a=3 : columns(x)+1);  % a = numbers of col
+   for(b=1 : rowReduce); % b = number of row
    px(b,a) = fxx(b,a,px,space);
   endfor
-  space = space+1
+  space = space+1;
   rowReduce = rowReduce-1; 
  endfor
  
- 
- 
-endfunction
+ endfunction
--- assignmentNumeric/assignmentTwo.m
+++ assignmentNumeric/assignmentTwo.m
@@ -2,5 +2,18 @@ x = [2.5,2.0,3.0,1.5,3.5,1.0,4.0,0.5,4.5,0.0,5.0];
 y = [0.68,0.66,0.90,0.79,1.31,1.02,1.77,1.30,1.87,1.50,1.33];
 ay = [0.70,0.74,0.94,0.79,1.34,1.05,1.78,1.33,1.87,1.49,1.31];
 
-DividedDiff(x,y)
+ matirx = DividedDiff(x,y)                 
+ matirxApproximate = DividedDiff(x,ay)
+  
+ nestedForm = @(x) NestedForm(matirx,x)
+ nestedFormApproximate = @(xAp) NestedForm(matirxApproximate,xAp)
+ 
+ coefficientNestedForm = matirx(1,2:columns(matirx))
+ coefficientNestedFormAppro = matirxApproximate(1,2:columns(matirxApproximate))
 
+## from here it's just on Matlsb caculation
+ degree = length(x) -1;
+ coeff = polyfit(x,y,degree);
+ fun = @(x) polyval(coeff,x);
+ fplot(nestedForm,[-0.1,5.1]);hold on;fplot(fun,[-0.1,5.1]);hold off
+ 
