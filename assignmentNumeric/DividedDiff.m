308d21fe2e69cb2aa9b80cdfe7f088e5f0105210
mahshidghaffari <72268533+mahshidghaffari@users.noreply.github.com>
2021-05-04 12:50:09 +0200

    remove matlab error

--- assignmentNumeric/DividedDiff.m
+++ assignmentNumeric/DividedDiff.m
@@ -1,25 +1,25 @@
-## Author: Mah <mah@HP>
-## Created: 2021-04-27
+% Author: Mah <mah@HP>
+% Created: 2021-04-27
 
-function px = DividedDiff (x,y);
- px = NaN(columns(x),columns(x)+1);
+function px = DividedDiff (x,y)
+ px = NaN(length(x),length(x)+1);
  space = 1;
  fxx = @(row,col,px,space) (px(row+1,col-1)-px(row,col-1))/( x(row+space)-x(row));  
  
- ##filling the first two col with inputs
- for (i=1 : columns(x));
+ %filling the first two col with inputs
+ for i=1 : length(x)
     px(i,1) = x(i);  
     px(i,2) = y(i);
- endfor
+ end
  
- ## using the divided diffrence for caculates other cols
- rowReduce =  columns(x)-1;
- for (a=3 : columns(x)+1);  % a = numbers of col
-   for(b=1 : rowReduce); % b = number of row
+ % using the divided diffrence for caculates other cols
+ rowReduce =  length(x)-1;
+ for a=3 : length(x)+1  % a = numbers of col
+   for b=1 : rowReduce % b = number of row
    px(b,a) = fxx(b,a,px,space);
-  endfor
+   end
   space = space+1;
   rowReduce = rowReduce-1; 
- endfor
+ end
  
- endfunction
+end
