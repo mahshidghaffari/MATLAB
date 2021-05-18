83aaab9576404c38b03b30e1e292f5f9e9121873
mahshidghaffari <72268533+mahshidghaffari@users.noreply.github.com>
2021-05-18 23:23:16 +0200

    Update TrigonometricPolynomial.m
    remove MTLAB error

--- assignmentNumeric/TrigonometricPolynomial.m
+++ assignmentNumeric/TrigonometricPolynomial.m
@@ -1,10 +1,8 @@
-
-
 function SF = TrigonometricPolynomial (ca, cb)
 
   SF = @(x) ca(1)/2;
     for j = 2:length(cb)+1
       SF = @(x) SF(x) + ca(j) * cos((j-1)*x);
       SF = @(x) SF(x) + cb(j-1) * sin((j-1)*x);
-  end
+    end
 end
