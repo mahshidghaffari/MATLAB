28e58ca8727318ef6ff836c7ab0d9b43d4504f7d
mahshidghaffari <72268533+mahshidghaffari@users.noreply.github.com>
2021-05-18 23:24:25 +0200

    Update FourierCoef.m
    remove MATLAB error

--- assignmentNumeric/FourierCoef.m
+++ assignmentNumeric/FourierCoef.m
@@ -1,13 +1,13 @@
-
-## Author: Mah <mah@HP>
-## Created: 2021-05-17
+% Author: Mah <mah@HP>
+% Created: 2021-05-17
 
 function [ca,cb] = FourierCoef (f, n)
-  
+  ca = zeros(1, n+1);
+  cb = zeros(1, n);
   for k=1:n+1
     ca(k) = (1/pi)*Simpson(-pi,pi,@(x) f(x) * cos((k-1)*x));
   
-    if k != n+1
+    if k ~= n+1
       cb(k) = (1/pi) * Simpson(-pi,pi,@(x) f(x) * sin((k)*x)); 
     end
     
